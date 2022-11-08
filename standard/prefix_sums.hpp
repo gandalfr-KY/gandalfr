3300e8294a846310cd3ceb5113242ecace18774d
gandalfr-KY <yamauchi@LAPTOP-TQAH7UBI.localdomain>
2022-11-08 17:31:53 +0900

    ✨

---
+++ standard/prefix_sums.hpp
@@ -0,0 +1,19 @@
+#ifndef COMPRESS
+#define COMPRESS
+#include <vector>
+
+/* 長さ v.size() + 1 の累積和vectorを返す
+ * Σv[l, r) = prf[r] - prf[l] が成立
+ */
+template<class T>
+std::vector<T> prefix_sums(const std::vector<T> &v){
+    std::vector<T> ret = {0};
+    T acm = 0;
+    for(T x : v){
+        acm += x;
+        ret.push_back(acm);
+    }
+    return ret;
+}
+
+#endif

78ba6e5c7d20bb992be9208c26e6f74f577c45a3
gandalfr-KY <yamauchi@LAPTOP-TQAH7UBI.localdomain>
2022-11-10 22:49:12 +0900

    🎉

--- graph/diameter.hpp
+++ graph/diameter.hpp
@@ -3,7 +3,6 @@
 #include "gandalfr/graph/shortest_path.hpp" 
 
 int diameter(unweighted_graph &graph){
-    assert(!graph.is_directed && graph.M == graph.N - 1);
     std::vector<int> dist = shortest_path(graph, 0);
     int farthest_node = std::max_element(dist.begin(), dist.end()) - dist.begin();
     dist = shortest_path(graph, farthest_node);
@@ -12,7 +11,6 @@ int diameter(unweighted_graph &graph){
 
 template<typename WEIGHT>
 WEIGHT diameter(weighted_graph<WEIGHT> &graph){
-    assert(!graph.is_directed && graph.M == graph.N - 1);
     std::vector<WEIGHT> dist = shortest_path(graph, 0);
     int farthest_node = std::max_element(dist.begin(), dist.end()) - dist.begin();
     dist = shortest_path(graph, farthest_node);

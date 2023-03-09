6ffb92976b73bb342236a7b854cc62b6d774edd0
gandalfr-KY <suraimuempera@gmail.com>
2023-03-09 23:47:42 +0900

    🐛

--- standard/fraction.hpp
+++ standard/fraction.hpp
@@ -2,10 +2,9 @@
 #define FRACTION
 #include "gandalfr/standard/gcdlcm.hpp"
 #include "gandalfr/math/integer/mod_inverse.hpp"
-#include "gandalfr/other/io_supporter.hpp"
 
 // verify : https://atcoder.jp/contests/abc168/submissions/39533747
-// 分子・分母がともに64bit整数の範囲でのみ動作を保証
+// 分子・分母がともに 64bit 整数の範囲でのみ動作を保証
 class fraction{
   private:
     __int128_t num, den;
@@ -39,25 +38,28 @@ class fraction{
 
     friend bool operator==(const fraction &a, const fraction &b){ return a.num == b.num && a.den == b.den; }
     friend bool operator!=(const fraction &a, const fraction &b){ return a.num != b.num || a.den != b.den; }
-    friend bool operator>(const fraction &a, const fraction &b) { return a.num * b.den >  b.num * a.den; }
-    friend bool operator>=(const fraction &a, const fraction &b){ return a.num * b.den >= b.num * a.den; }
-    friend bool operator<(const fraction &a, const fraction &b) { return a.num * b.den <  b.num * a.den; }
-    friend bool operator<=(const fraction &a, const fraction &b){ return a.num * b.den <= b.num * a.den; }
+    friend bool operator>(const fraction &a, const fraction &b) {
+        if((a.num >= 0) ^ (b.num >= 0)) return (a.num > b.num);
+        return a.num * b.den > b.num * a.den;
+    }
+    friend bool operator>=(const fraction &a, const fraction &b) {
+        if((a.num >= 0) ^ (b.num >= 0)) return (a.num >= b.num);
+        return a.num * b.den >= b.num * a.den;
+    }
+    friend bool operator<(const fraction &a, const fraction &b) {
+        if((a.num >= 0) ^ (b.num >= 0)) return (a.num < b.num);
+        return a.num * b.den < b.num * a.den;
+    }
+    friend bool operator<=(const fraction &a, const fraction &b){
+        if((a.num >= 0) ^ (b.num >= 0)) return (a.num <= b.num);
+        return a.num * b.den <= b.num * a.den;
+    }
 
   public:
     fraction(const fraction &a){ num = a.num, den = a.den; }
     fraction(__int128_t n) : num(n), den(1) {}
     // false を添えると約分されない
     fraction(__int128_t numerator, __int128_t denominator, bool with_simplify = true) : num(numerator), den(denominator) { if(with_simplify) simplify(); }
-    fraction(const std::string &buf) : num(0), den(0) {
-        int i = (buf[0] == '-');
-        for(; i < buf.size() && buf[i] != '/'; i++) num = num * 10 + buf[i] - '0';
-        if(i == buf.size()) den = 1;
-        else for(i = i + 1; i < buf.size(); i++) den = den * 10 + buf[i] - '0';
-        if(buf[0] == '-') num *= -1;
-        simplify();
-    }
-    fraction(const char s[]) : fraction(std::string(s)) {}
     fraction() : num(0), den(1) {}
 
     fraction &operator=(const fraction &a){
@@ -105,18 +107,16 @@ class fraction{
         return is;
     }
     friend std::ostream &operator<<(std::ostream &os, const fraction &a) {
-        if(a.den == 0){
-            os << (a.num >= 0 ? "inf" : "-inf");
-        }
-        else{
-            os << a.num;
-            if(a.den != 1) os << '/' << a.den;
-        }
+        if(a.den == 0) os << (a.num >= 0 ? "inf" : "-inf");
+        else if(a.den == 1) os << (long long)a.num;
+        else os << (long long)a.num << '/' << (long long)a.den;
         return os;
     }
 
     long long numerator() const { return num; }
     long long denomitnator() const { return den; }
+    long long floor() const { return num / den; }
+    long long ceil() const { return (num + den - 1) / den; }
     double real() const { return (double)num / den; }
     fraction abs() const { return {(num >= 0 ? num : -num), den, false}; } 
     fraction inverse() const { return {(num >= 0 ? den : -den), (num >= 0 ? num : -num), false}; }
@@ -128,11 +128,9 @@ class fraction{
         return ret;
     }
 
-    static const fraction M_INF;
-    static const fraction INF;
+    static const fraction M_INF, INF;
 };
-const fraction fraction::M_INF("-1/0");
-const fraction fraction::INF("1/0");
+const fraction fraction::M_INF(-1, 0), fraction::INF(1, 0);
 
 
 #endif

ad9485cae60577d62bbd411e07e676d90f4b86c7
gandalfr-KY <yamauchi@LAPTOP-TQAH7UBI.localdomain>
2023-04-10 16:10:47 +0900

    ♻

--- graph/Warshall_Floyd.hpp
+++ graph/Warshall_Floyd.hpp
@@ -4,20 +4,16 @@
 #include "gandalfr/math/matrix/matrix.hpp"
 
 template<class T>
-matrix<T> Warshall_Floyd(const matrix<T> &mt, T invalid = -1) {
+matrix<T> Warshall_Floyd(matrix<T> mt) {
     int N = mt.size_H();
-    matrix<T> ret(mt);
     for(int k = 0; k < N; k++){ // 経由する頂点
         for(int i = 0; i < N; i++) { // 始点
-            if(ret[i][k] == invalid) continue;
             for(int j = 0; j < N; j++) { // 終点
-                if(ret[k][j] == invalid) continue;
-                if(ret[i][j] == invalid) ret[i][j] = ret[i][k] + ret[k][j];
-                else ret[i][j] = std::min(ret[i][j], ret[i][k] + ret[k][j]);
+                mt[i][j] = std::min(mt[i][j], mt[i][k] + mt[k][j]);
             }
         }
     }
-    return ret;
+    return mt;
 }
 
 #endif

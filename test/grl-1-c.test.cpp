c8e052bd79732198955f5f4a130370004a4bb4b3
gandalfr-KY <suraimuempera@gmail.com>
2023-06-07 00:16:34 +0900

    added wf-test

--- test/grl-1-c.test.cpp
+++ test/grl-1-c.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_1_C"
 #include <bits/stdc++.h>
-#include "../graph/shortest_path.hpp"
+#include "../graph/Warshall_Floyd.hpp"
 using namespace std;
 using ll = long long;
 #define rep(i, j, n) for(ll i = (ll)(j); i < (ll)(n); i++)
@@ -20,12 +20,11 @@ int main(void){
  
     //calculate
     
-    vector<vector<ll>> dist(N);
-    rep(i,0,N) dist[i] = shortest_path(G, i);
+    auto dist = Warshall_Floyd(matrix(G, 1000000000LL));
 
     rep(i,0,N){
         rep(j,0,N){
-            cout << (dist[i][j] == -1 ? "INF" : to_string(dist[i][j])) << " ";
+            cout << (dist[i][j] == -1 ? "INF" : to_string(dist[i][j])) << (j == N - 1 ? '\n' : ' ');
         }
         cout << endl;
     }

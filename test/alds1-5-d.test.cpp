3f31843b28142531fdd2f965ea51a8d27572489a
gandalfr-KY <suraimuempera@gmail.com>
2023-06-08 21:00:42 +0900

    inversioon test

--- test/alds1-5-d.test.cpp
+++ test/alds1-5-d.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://atcoder.jp/contests/abc296/tasks/abc296_f"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_5_D"
 #include <bits/stdc++.h>
 #include "../standard/inversion.hpp"
 #include "../other/io_supporter.hpp"
@@ -19,26 +19,11 @@ int main(void){
  
     int N;
     cin >> N;
-    vector<int> A(N), B(N);
+    vector<int> A(N);
     cin >> A;
-    cin >> B;
-    vector<int> _A(A), _B(B);
-    sort(all(_A));
-    sort(all(_B));
-    if(_A != _B){
-        Yes(false);
-        return 0;
-    }
- 
+
     //calculate
     
-    rep(i,0,N-1){
-        if(_A[i] == _A[i+1]){
-            Yes(true);
-            return 0;
-        }
-    }
- 
-    Yes(inversion(all(A)) % 2 == inversion(all(B)) % 2);
+    cout << inversion(A.begin(), A.end()) << endl;
  
 }

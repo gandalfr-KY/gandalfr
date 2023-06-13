9af6220414240ab76d663535db7ac41d8dde9792
gandalfr-KY <suraimuempera@gmail.com>
2023-06-13 23:12:39 +0900

    素数らへんいじった

--- math/prime_number_utility.hpp
+++ math/prime_number_utility.hpp
@@ -8,33 +8,30 @@
  */
 class prime_number_utility{
   protected:
-    static bool has_table_made;
-    static std::vector<bool> sieve;
-    static const int sieve_size = (1 << 18);
+    static const int init_seive_size = (1 << 24), sqrt_size = (1 << 12);
+    static inline std::vector<bool> sieve;
 
   public:
     prime_number_utility() = delete;
     ~prime_number_utility() = delete;
 
-    static bool is_prime(long long n){
-        if(!has_table_made){
-            sieve.assign(sieve_size, true);
+    static bool is_prime(long long n) {
+        if(sieve.empty()){ // 篩が未構築なら
+            sieve.assign(init_seive_size, true);
             sieve[0] = sieve[1] = false;
-            sieve[2] = true;
-            for(int i = 4; i < sieve_size; i += 2) sieve[i] = false;
-            int sqrt_size = ceil(std::sqrt(sieve_size)) + 1;
-            for(int i = 3; i <= sqrt_size; i += 2){
+            for(int i = 4; i < init_seive_size; i += 2) sieve[i] = false;
+            for(int i = 3; i <= sqrt_size; i += 2) {
                 if(!sieve[i]) continue;
-                for(int j = i * 2; j < sieve_size; j += i) sieve[j] = false;
+                for(int j = i * 2; j < init_seive_size; j += i) sieve[j] = false;
             }
-            has_table_made = true;
         }
-        if(n < 0) n *= -1;
-        if(n < sieve_size) return sieve[n];
-        
-        long long sqrt_n = ceil(std::sqrt(n)) + 1;
-        if(n % 2 == 0) return false;
-        for(long long i = 3; i <= sqrt_n; i += 2){
+
+        // n が小さいとき、篩を参照
+        if(n < init_seive_size) return sieve[n];
+        if(n & 1 == 0) return 0;
+        // n が大きいとき、O(√n) 試し割りで計算
+        long long sqrt_n = std::ceil(std::sqrt(n)) + 1;
+        for(long long i = 3; i <= sqrt_n; i += 2) {
             if(!sieve[i]) continue;
             if(n % i == 0) return false;
         }
@@ -43,8 +40,6 @@ class prime_number_utility{
 
     static const std::vector<bool> &table(){ return sieve; }
 };
-std::vector<bool> prime_number_utility::sieve;
-bool prime_number_utility::has_table_made = false;
 
 
 #endif

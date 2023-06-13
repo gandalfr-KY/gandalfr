ea8f5da1420a67b7fa9cefa77ebd7dc3d4a77f24
gandalfr-KY <suraimuempera@gmail.com>
2023-06-13 14:30:53 +0900

    ロリハを安全にした

--- standard/rolling_hash.hpp
+++ standard/rolling_hash.hpp
@@ -2,21 +2,63 @@
 #define ROLLING_HASH
 #include <string>
 #include <vector>
+#include <random>
 
 /**
  * @brief rolling_hashアルゴリズムで文字列を管理するクラス
  */
 class rolling_hash{
 private:
-    const __uint128_t _base = 10007, _mod = ((__uint128_t)1<<61) - 1;
-	std::vector<__uint128_t> hashes, bases; 
+    unsigned long long _base;
+    const unsigned long long _mod = (1LL<<61) - 1;
+	std::vector<unsigned long long> hashes, bases;
+    static inline const std::vector<unsigned long long> base_list{
+        1809535154732661841LL,
+        1884190988654199289LL,
+        1743269039721525290LL,
+        443670500607027996LL,
+        1847920912542467715LL,
+        854073078368186965LL,
+        1223509603385983965LL,
+        1417510665238831134LL,
+        1078954822254450931LL,
+        544731273123194097LL,
+        628194563995444860LL,
+        2068028567818163032LL,
+        382415649034687899LL,
+        1278119205153488676LL,
+        1191116173253440671LL,
+        1896252179888216214LL,
+        1559071116560504239LL,
+        924438638735228819LL,
+        1857807495571573189LL,
+        307827764609342932LL,
+        152462581942784977LL
+    };
 
 public:
 	rolling_hash(const std::string &s) : hashes(s.size() + 1, 0), bases(s.size() + 1, 0) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distr(0, base_list.size() - 1);
+        _base = base_list[distr(gen)]; // generate numbers
+
 		bases[0] = 1;
 		for(int i = 0; i < (int)s.size(); i++){
-			hashes[i + 1] = (hashes[i] * _base + s[i]) % _mod;
-			bases[i + 1] = (bases[i] * _base) % _mod;
+			hashes[i + 1] = ((__uint128_t)hashes[i] * _base + s[i]) % _mod;
+			bases[i + 1] = ((__uint128_t)bases[i] * _base) % _mod;
+		}
+	}
+
+    /**
+     * @brief 基数、mod を手動で設定するとき
+     */
+	rolling_hash(const std::string &s, unsigned long long b, unsigned long long m)
+        : _base(b), _mod(m), hashes(s.size() + 1, 0), bases(s.size() + 1, 0) {
+		bases[0] = 1;
+		for(int i = 0; i < (int)s.size(); i++){
+			hashes[i + 1] = ((__uint128_t)hashes[i] * _base + s[i]) % _mod;
+			bases[i + 1] = ((__uint128_t)bases[i] * _base) % _mod;
 		}
 	}
 
@@ -24,16 +66,16 @@ public:
      * @brief 半開区間を指定
      * @return string[l, r) のハッシュ値
      */
-	__uint128_t get(int l, int r){
-		return (hashes[r] + _mod - (hashes[l] * bases[r - l]) % _mod) % _mod;
+	unsigned long long get(int l, int r){
+		return (hashes[r] + _mod - ((__uint128_t)hashes[l] * bases[r - l]) % _mod) % _mod;
 	}
 
 	/**
      * @brief 文字列を結合してハッシュ値を計算
      * @return get(_l, _r) == r_hash なる string[_l, _r) に対して、[_l, _r) + [l, r) のハッシュ値
      */
-    __uint128_t concat(__uint128_t r_hash, int l, int r){
-        return (r_hash * bases[r - l] + get(l, r)) % _mod;
+    unsigned long long concat(__uint128_t r_hash, int l, int r){
+        return ((__uint128_t)r_hash * bases[r - l] + get(l, r)) % _mod;
 	}
 
     /**
@@ -48,6 +90,7 @@ public:
         }
         return ok;
     }
+
 };
 
 #endif

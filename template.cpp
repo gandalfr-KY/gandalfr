edb04d894877874a1f8ac9695bf5f67ff11d6d78
gandalfr-KY <suraimuempera@gmail.com>
2023-08-22 16:01:20 +0900

    テンプレート変更

--- template.cpp
+++ template.cpp
@@ -3,11 +3,7 @@
 using namespace std;
 using ll = long long;
 const int INF = 1001001001;
-const int MAXINT = std::numeric_limits<int>::max();
-const int MININT = std::numeric_limits<int>::min();
 const ll INFLL = 1001001001001001001;
-const ll MAXLL = std::numeric_limits<ll>::max();
-const ll MINLL = std::numeric_limits<ll>::min();
 const ll MOD  = 1000000007;
 const ll _MOD = 998244353;
 #define rep(i, j, n) for(ll i = (ll)(j); i < (ll)(n); i++)

4634cf45e7651287879ff4b898e03c65e9aeb6a1
gandalfr-ky <oukanimiso@gmail.com>
2024-03-23 20:42:07 +0900

    a

--- standard/Mint.hpp
+++ standard/Mint.hpp
@@ -8,46 +8,26 @@ namespace gandalfr {
 
 using Mint998 = atcoder::modint998244353;
 using Mint107 = atcoder::modint1000000007;
-template <int m> using Mint = atcoder::static_modint<m>;
-template <int id> using DMint = atcoder::dynamic_modint<id>;
+template <i32 m> using Mint = atcoder::static_modint<m>;
+template <i32 id> using DMint = atcoder::dynamic_modint<id>;
 
-template <int m> std::ostream &operator<<(std::ostream &os, const Mint998 &mi) {
+template <i32 m> std::ostream &operator<<(std::ostream &os, const Mint<m> &mi) {
     os << mi.val();
     return os;
 }
-template <int m> std::ostream &operator<<(std::ostream &os, const Mint107 &mi) {
-    os << mi.val();
-    return os;
-}
-template <int m> std::ostream &operator<<(std::ostream &os, const Mint<m> &mi) {
-    os << mi.val();
-    return os;
-}
-template <int id>
+template <i32 id>
 std::ostream &operator<<(std::ostream &os, const DMint<id> &mi) {
     os << mi.val();
     return os;
 }
 
-template <int m> std::istream &operator>>(std::istream &is, Mint998 &mi) {
+template <i32 m> std::istream &operator>>(std::istream &is, Mint<m> &mi) {
     long long n;
     is >> n;
     mi = n;
     return is;
 }
-template <int m> std::istream &operator>>(std::istream &is, Mint107 &mi) {
-    long long n;
-    is >> n;
-    mi = n;
-    return is;
-}
-template <int m> std::istream &operator>>(std::istream &is, Mint<m> &mi) {
-    long long n;
-    is >> n;
-    mi = n;
-    return is;
-}
-template <int id> std::istream &operator>>(std::istream &is, DMint<id> &mi) {
+template <i32 id> std::istream &operator>>(std::istream &is, DMint<id> &mi) {
     long long n;
     is >> mi;
     mi = n;

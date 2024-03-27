984ec209d3c6ff85769c227a5ead013bdebcdd70
gandalfr-ky <oukanimiso@gmail.com>
2024-03-28 02:59:42 +0900

    Bnary trie

--- data_structure/BinaryTrie.hpp
+++ data_structure/BinaryTrie.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <assert.h>
 #include "gandalfr/types.hpp"
 
 namespace gandalfr {
@@ -10,62 +11,131 @@ template <u32 bit_width> struct BinaryTrie {
 
   private:
     struct BinaryTrieNode {
-        std::shared_ptr<BinaryTrieNode> child[2] = {nullptr, nullptr};
-        std::shared_ptr<BinaryTrieNode> par = nullptr;
-        u32 level;
+        std::shared_ptr<BinaryTrieNode> children[2] = {nullptr, nullptr};
+        u32 level, sub_cnt = 0;
         BinaryTrieNode(u32 lvl) : level(lvl) {}
-        u32 num_children() {
-            if (child[0] && child[1])
-                return 2;
-            else if (child[0] || child[1])
-                return 1;
-            else
-                return 0;
+        // 00: xx, 01: xo, 10: ox, 11: oo
+        u32 stateOfChildren() const {
+            return ((bool)children[1] << 1) | (bool)children[0];
+        }
+        bool isLeaf() const {
+            return level == 0;
+        }
+        bool nextIndex(u64 n) const {
+            return (n >> (level - 1)) & 1;
         }
     };
-    std::shared_ptr<BinaryTrieNode> root =
-        std::make_shared<BinaryTrieNode>(bit_width - 1);
+    using NodePtr = std::shared_ptr<BinaryTrieNode>;
+    NodePtr root = std::make_shared<BinaryTrieNode>(bit_width);
+    // u64 xorval = 0;
+
+    NodePtr getNodePtr(u64 n) const {
+        NodePtr cur = root;
+        while (!cur->isLeaf()) {
+            bool b = cur->nextIndex(n);
+            if (!cur->children[b]) {
+                return nullptr;
+            }
+            cur = cur->children[b];
+        }
+        return cur;
+    }
 
   public:
     void insert(u64 n) {
-        auto cur = root;
+        NodePtr cur = root;
         while (cur->level) {
-            bool b = (n >> cur->level) & 1;
-            if (!cur->child[b]) {
-                cur->child[b] =
+            cur->sub_cnt += 1;
+            bool b = cur->nextIndex(n);
+            if (!cur->children[b]) {
+                cur->children[b] =
                     std::make_shared<BinaryTrieNode>(cur->level - 1);
-                cur->child[b]->par = cur;
             }
-            cur = cur->child[b];
+            cur = cur->children[b];
         }
+        cur->sub_cnt += 1;
     }
 
-    u32 count(u64 n) {
-        auto cur = root;
-        while (cur->level) {
-            bool b = (n >> cur->level) & 1;
-            if (!cur->child[b]) {
-                return 0;
-            }
-            cur = cur->child[b];
-        }
-        return 1;
+    u32 size() const {
+        return root->sub_cnt;
     }
 
-    void erase(u64 n) {
-        auto cur = root;
-        while (cur->level) {
-            bool b = (n >> cur->level) & 1;
-            if (!cur->child[b]) {
+    u32 count(u64 n) const {
+        NodePtr ptr = getNodePtr(n);
+        return (ptr ? ptr->sub_cnt : 0);
+    }
+
+    void erase(u64 n) const {
+        u32 cnt = count(n);
+        if (cnt == 0) return;
+        NodePtr cur = root;
+        while (true) {
+            cur->sub_cnt -= cnt;
+            bool b = cur->nextIndex(n);
+            if (cur->children[b]->sub_cnt == cnt) {
+                cur->children[b] = nullptr;
                 return;
             }
-            cur = cur->child[b];
-        }
-        while (cur->par && !cur.num_children()) {
-            auto tmp = cur;
-            cur = cur->par;
-            tmp.reset();
+            cur = cur->children[b];
         }
     }
+
+    void eraseOne(u64 n) const {
+        if (count(n) == 0) return;
+        NodePtr cur = root;
+        while (!cur->isLeaf()) {
+            cur->sub_cnt -= 1;
+            bool b = cur->nextIndex(n);
+            if (cur->children[b]->sub_cnt == 1) {
+                cur->children[b] = nullptr;
+                return;
+            }
+            cur = cur->children[b];
+        }
+        cur->sub_cnt -= 1;
+    }
+
+    // n: 0-indexed
+    u64 nthElement(u32 n) const {
+        assert(n < size());
+        u64 ret = 0;
+        NodePtr cur = root;
+        while (!cur->isLeaf()) {
+            ret <<= 1;
+            u32 state = cur->stateOfChildren();
+            assert(state > 0);
+            if (state == 1 || (state == 3 && n < cur->children[0]->sub_cnt)) {
+                cur = cur->children[0];
+            } else {
+                n -= (state & 1 ? cur->children[0]->sub_cnt : 0);
+                ret |= 1;
+                cur = cur->children[1];
+            }
+        }
+        return ret;
+    }
+
+    // https://atcoder.jp/contests/abc312/submissions/51738375
+    u32 lowerBound(u64 n) const {
+        u32 ret = 0;
+        NodePtr cur = root;
+        while (!cur->isLeaf()) {
+            bool b = cur->nextIndex(n);
+            if (b && cur->children[0]) {
+                ret += cur->children[0]->sub_cnt;
+            }
+            if (!cur->children[b]) {
+                break;
+            }
+            cur = cur->children[b];
+        }
+        return ret;
+    }
+
+    u32 upperBound(u64 n) const {
+        return lowerBound(n + 1);
+    }
+
+
 };
 } // namespace gandalfr

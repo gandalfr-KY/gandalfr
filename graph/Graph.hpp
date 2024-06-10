3ca8ce72918ea45081f2ba5cfe6258088cd2c8ad
gandalfr-KY <oukanimiso@gmail.com>
2024-06-10 09:49:16 +0900

    graph discomponent

--- graph/Graph.hpp
+++ graph/Graph.hpp
@@ -456,5 +456,39 @@ template <bool is_weighted, bool is_directed> class Graph {
         }
         return true;
     }
+
+    /**
+     * @brief 連結成分ごとに分解
+     * @return {分解後のグラフ、grp_id, nd_id}
+     */
+    std::tuple<std::vector<Graph>, std::vector<i32>, std::vector<i32>> discomponent() const {
+        UnionFind uf(N);
+        for (auto &e : E) {
+            uf.unite(e->v0, e->v1);
+        }
+        i32 n_grps = uf.numGroups();
+        auto grps = uf.getAllGroups();
+
+        std::vector<i32> grp_id(N), nd_id(N);
+        for (i32 i = 0; i < n_grps; ++i) {
+            for (i32 j = 0; j < (i32)grps[i].size(); ++j) {
+                grp_id[grps[i][j]] = i;
+                nd_id[grps[i][j]] = j;
+            }
+        }
+
+        std::vector<Graph> Gs(n_grps);
+        for (i32 i = 0; i < n_grps; ++i) {
+            Gs[i].resize(grps[i].size());
+        } 
+        for (auto &e : E) {
+            i32 id = grp_id[e->v0];
+            i32 u = nd_id[e->v0];
+            i32 v = nd_id[e->v1];
+            Gs[id].addEdge(u, v);
+        }
+        return {Gs, grp_id, nd_id};
+    }
+
 };
 } // namespace gandalfr
